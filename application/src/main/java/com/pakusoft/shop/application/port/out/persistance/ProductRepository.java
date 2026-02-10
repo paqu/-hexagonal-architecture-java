3baa278b67e126dac8facd1ca23402d09bcb103e
Pawel Kusz <pawel.kusz1993@gmail.com>
2026-02-10 21:51:09 +0100

    feat(application): add interface for Product repo

--- application/src/main/java/com/pakusoft/shop/application/port/out/persistance/ProductRepository.java
+++ application/src/main/java/com/pakusoft/shop/application/port/out/persistance/ProductRepository.java
@@ -1,4 +1,16 @@
 package com.pakusoft.shop.application.port.out.persistance;
 
+import com.pakusoft.shop.model.product.Product;
+import com.pakusoft.shop.model.product.ProductId;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductRepository {
+
+    void save(Product product);
+
+    Optional<Product> findById(ProductId productId);
+
+    List<Product> findByNameOrDescription(String query);
 }
