1a9f4ca917ff426ee996163dac8a57701dcfdc1b
Pawel Kusz <pawel.kusz1993@gmail.com>
2026-02-18 23:14:28 +0100

    test(adapter): use Testcontainers for JPA repository integration tests

--- adapter/src/test/java/com/pakusoft/shop/adapter/out/persistence/jpa/JpaCartRepositoryTest.java
+++ adapter/src/test/java/com/pakusoft/shop/adapter/out/persistence/jpa/JpaCartRepositoryTest.java
@@ -2,9 +2,33 @@ package com.pakusoft.shop.adapter.out.persistence.jpa;
 
 import com.pakusoft.shop.adapter.out.persistence.AbstractCartRepositoryTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@SpringBootTest(
+        classes = com.pakusoft.shop.adapter.SpringAppConfig.class
+)
+@Testcontainers
+class JpaCartRepositoryTest extends AbstractCartRepositoryTest {
 
-@SpringBootTest
-@ActiveProfiles("test-with-mysql")
-class JpaCartRepositoryTest extends AbstractCartRepositoryTest {}
+    @Container
+    static final MySQLContainer<?> mysql =
+            new MySQLContainer<>("mysql:8.0")
+                    .withDatabaseName("shop")
+                    .withUsername("test")
+                    .withPassword("test");
+
+    @DynamicPropertySource
+    static void configureDatasource(DynamicPropertyRegistry registry) {
+
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+        registry.add("spring.datasource.driver-class-name", mysql::getDriverClassName);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+
+    }
+}
--- adapter/src/test/java/com/pakusoft/shop/adapter/out/persistence/jpa/JpaProductRepositoryTest.java
+++ adapter/src/test/java/com/pakusoft/shop/adapter/out/persistence/jpa/JpaProductRepositoryTest.java
@@ -2,10 +2,31 @@ package com.pakusoft.shop.adapter.out.persistence.jpa;
 
 import com.pakusoft.shop.adapter.out.persistence.AbstractProductRepositoryTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@SpringBootTest(
+        classes = com.pakusoft.shop.adapter.SpringAppConfig.class
+)
+@Testcontainers
+class JpaProductRepositoryTest extends AbstractProductRepositoryTest {
 
+    @Container
+    static MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:8.0")
+            .withDatabaseName("shop")
+            .withUsername("test")
+            .withPassword("test");
 
-@SpringBootTest
-@ActiveProfiles("test-with-mysql")
-class JpaProductRepositoryTest extends AbstractProductRepositoryTest {}
+    @DynamicPropertySource
+    static void overrideProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mysqlContainer::getUsername);
+        registry.add("spring.datasource.password", mysqlContainer::getPassword);
+        registry.add("spring.datasource.driver-class-name", mysqlContainer::getDriverClassName);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+    }
+}
+
