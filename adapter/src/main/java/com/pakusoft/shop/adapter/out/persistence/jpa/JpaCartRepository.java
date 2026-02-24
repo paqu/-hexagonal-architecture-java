6a6fde34bcecab535ed72e14fbf39c222c3c8bc8
Pawel Kusz <pawel.kusz1993@gmail.com>
2026-02-24 13:39:31 +0100

    feat(adapter): implement CartRepository JPA adapter with transactional support

--- adapter/src/main/java/com/pakusoft/shop/adapter/out/persistence/jpa/JpaCartRepository.java
+++ adapter/src/main/java/com/pakusoft/shop/adapter/out/persistence/jpa/JpaCartRepository.java
@@ -1,2 +1,39 @@
-package com.pakusoft.shop.adapter.out.persistence.jpa;public class JpaCartRepository {
+package com.pakusoft.shop.adapter.out.persistence.jpa;
+
+import com.pakusoft.shop.application.port.out.persistence.CartRepository;
+import com.pakusoft.shop.model.cart.Cart;
+import com.pakusoft.shop.model.customer.CustomerId;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+
+@Repository
+public class JpaCartRepository implements CartRepository {
+
+    private final JpaCartSpringDataRepository springDataRepository;
+
+    public JpaCartRepository(JpaCartSpringDataRepository springDataRepository) {
+        this.springDataRepository = springDataRepository;
+    }
+
+    @Override
+    @Transactional
+    public void save(Cart cart) {
+        springDataRepository.save(CartMapper.toJpaEntity(cart));
+    }
+
+    @Override
+    @Transactional
+    public Optional<Cart> findByCustomerId(CustomerId customerId) {
+        Optional<CartJpaEntity> cartJpaEntity = springDataRepository.findById(customerId.value());
+        return cartJpaEntity.map(CartMapper::toModelEntity);
+    }
+
+    @Override
+    @Transactional
+    public void deleteByCustomerId(CustomerId customerId) {
+        springDataRepository.deleteById(customerId.value());
+    }
 }
